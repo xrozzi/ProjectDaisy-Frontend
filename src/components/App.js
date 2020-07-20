1f5143f788d7b00f669ff2a58d6ab1d2017b3aab
Rose H <rocio.rosas16@gmail.com>
2020-07-20 10:18:41 +1000

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/ui/Header'
+
+
+function App() {
+  return (
+    <div className="App"> <Header /> Hello!</div>
+  );
+}
+
+export default App;
---
+++ src/components/ui/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
+import App from '../App';
+
+
+export default function Header(props) {
+    return (
+        <AppBar>
+            <Toolbar>
+                Development
+            </Toolbar>
+        </AppBar>
+    )
+}
+
