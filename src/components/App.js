22299a3855f7fa74ad080448118b0e8c99bde53e
Rose H <rocio.rosas16@gmail.com>
2020-07-20 11:42:41 +1000

    SignIn form

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from '../components/ui/Header'
+import SignIn from '../components/ui/SignIn'
 
 
 function App() {
   return (
-    <div className="App"> <Header /> Hello!</div>
+    <div className="App"> <Header /> Hello!
+    
+    <SignIn />
+    </div>
+    
   );
 }
+
 
 export default App;
