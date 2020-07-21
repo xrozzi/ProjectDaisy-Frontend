7cedf784bfe0e24ab0cf4060fd2d8a41cd8cbabd
esthercsoke <esthercsoke@gmail.com>
2020-07-21 11:56:11 +1000

    created mobile side bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,38 @@
-import React from 'react';
-import { ThemeProvider } from '@material-ui/core/styles';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import theme from './ui/Theme';
-import Header from '../components/ui/Header'
-
+import React from "react";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import theme from "./ui/Theme";
+import Header from "../components/ui/Header";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <Header /> 
+        <Header />
         <Switch>
           <Route exact path="/" component={() => <div>Home</div>} />
-          <Route exact path="/GitCollaborations" component={() => <div>Git collaborations</div>} />
+          <Route
+            exact
+            path="/GitCollaborations"
+            component={() => <div>Git collaborations</div>}
+          />
           <Route exact path="/Forums" component={() => <div>Forums</div>} />
           <Route exact path="/Meetups" component={() => <div>Meetups</div>} />
           <Route exact path="/Login" component={() => <div>Meetups</div>} />
           <Route exact path="/SignUp" component={() => <div>Login</div>} />
-          <Route exact path="/CreateGitListing" component={() => <div>Create Git Listing</div>} />
-          <Route exact path="/AboutGitCollabs" component={() => <div>What is a Git Collab?</div>} />
-
-
-      
+          <Route
+            exact
+            path="/CreateGitListing"
+            component={() => <div>Create Git Listing</div>}
+          />
+          <Route
+            exact
+            path="/AboutGitCollabs"
+            component={() => <div>What is a Git Collab?</div>}
+          />
         </Switch>
       </BrowserRouter>
     </ThemeProvider>
-
   );
 }
 
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,246 +1,434 @@
-import React, {useState, useEffect} from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar'
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import { makeStyles } from '@material-ui/styles'
-import Tabs from '@material-ui/core/tabs'
-import Tab from '@material-ui/core/tab'
-import Button from '@material-ui/core/button'
-import {Link} from 'react-router-dom'
-import Menu from '@material-ui/core/Menu'
-import MenuItem from '@material-ui/core/MenuItem'
+import React, { useState, useEffect } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import { makeStyles } from "@material-ui/styles";
+import Tabs from "@material-ui/core/tabs";
+import Tab from "@material-ui/core/tab";
+import Button from "@material-ui/core/button";
+import { Link } from "react-router-dom";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { useTheme } from "@material-ui/styles";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
-import logo from '../../assets/logo.svg'
+import logo from "../../assets/logo.svg";
+// import { IconButton } from '@material-ui/core';
 
 function ElevationScroll(props) {
-    const { children } = props;
-  
-    const trigger = useScrollTrigger({
-      disableHysteresis: true,
-      threshold: 0,
-    });
-  
-    return React.cloneElement(children, {
-      elevation: trigger ? 4 : 0,
-    });
-  }
+  const { children } = props;
 
-  const useStyles = makeStyles(theme => ({
-      toolbarMargin: {
-          ...theme.mixins.toolbar,
-          marginBottom: "3em"
-      },
-      logo: {
-          height: "4em"
-      },
-      logoContainer: {
-          padding: 0,
-          "&:hover": {
-              backgroundColor: "transparent"
-          }
-      },
-      tabContainer: {
-          marginLeft: 'auto'
-      },
-      tab: {
-          ...theme.typography.tab,
-          minWidth: 10,
-          marginLeft: "25px"
-      }, 
-      button: {
-          ...theme.typography.signup,
-          borderRadius: "50px",
-          marginLeft: "50 px",
-          marginRight: "20px",
-          height: "45 px"
-      },
-      menu: {
-          backgroundColor: theme.palette.common.arcPurple,
-          color: "white",
-          borderRadius: "0px"
-      }, 
-      menuItem: {
-          ...theme.typography.tab,
-          opacity: 0.7,
-          "&:hover": {
-              opacity: 1
-          }
-      }
-    
-  }));
-  
-export default function Header(props) {
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
 
-    const classes = useStyles()
-    const [value, setValue] = useState(0)
-    const [anchorEl, setAnchorEl] = useState(null)
-    const [open, setOpen] = useState(false)
-    const [selectedIndex, setSelectedIndex] = useState(0)
-
-
-    const handleChange = (e, value) => {
-        setValue(value)
-    }
-
-    const handleClick = (e) => {
-        setAnchorEl(e.currentTarget)
-        setOpen(true)
-    }
-
-    const handleMenuItemClick = (e,i) => {
-        setAnchorEl(null)
-        setOpen(false)
-        setSelectedIndex(i)
-    }
-
-    const handleClose = (e) => {
-        setAnchorEl(null)
-        setOpen(false)
-    }
-
-    const menuOptions = [
-        {name: "Git Collaborations", link: "/GitCollaborations"}, 
-        {name: "Create Git Listing", link: "/CreateGitListing"},
-        {name: "What are Git Collabs?", link: "/AboutGitCollabs"}
-    ]
-
-
-
-    useEffect(() => {
-        if (window.location.pathname === '/' && value !== 0) {
-            setValue(0)
-        } else if (window.location.pathname === '/GitCollaborations' && value !== 1) {
-            setValue(1)
-        }  else if (window.location.pathname === '/Forums' && value !== 2) {
-            setValue(2)
-    } else if (window.location.pathname === '/Meetups' && value !== 3) {
-        setValue(3)
-     }
-     switch (window.location.pathname) {
-         case "/":
-            if (value !== 0) {
-                setValue(0)
-            }
-            break
-         
-         case "/GitCollaborations": 
-            if (value !== 1) {
-                setValue(1)
-                setSelectedIndex(0)
-                }
-               break
-        
-        case "/CreateGitListing": 
-            if (value !== 1) {
-                setValue(1)
-                setSelectedIndex(1)
-            }
-            break
-        
-        case "/AboutGitCollabs": 
-            if (value !== 1) {
-                setValue(1)
-                setSelectedIndex(2)
-                }
-                break
-        
-        case "/Forums": 
-            if (value !== 2) {
-                setValue(2)
-            }
-            break
-        
-        case "/Meetups": 
-            if (value !== 3) {
-                setValue(3)
-            }
-            break
-        
-            default: 
-            break
-     }
-    }, [value])
-
-    return (
-        <React.Fragment>
-            <ElevationScroll>
-                <AppBar position="fixed" color="primary">
-                    <Toolbar disableGutters>
-
-                        <Button component={Link} to="/"
-                        disableRipple
-                        onClick={() => 
-                            setValue(0)}
-                        className={classes.logoContainer}>
-                        <img alt="company logo" className={classes.logo} src={logo} />
-                        </Button>
-
-                      <Tabs value={value} onChange={handleChange} className={classes.tabContainer}>
-
-                         <Tab 
-                         className={classes.tab} 
-                         component={Link} to ="/" 
-                         label="Home" 
-                         /> 
-
-                         <Tab 
-                         aria-owns={anchorEl ? "simple menu" : undefined}
-                         aria-haspopup={anchorEl ? "true" : undefined}
-                         className={classes.tab} 
-                         component={Link} to ="/GitCollaborations" 
-                         onMouseOver={event => handleClick(event)}
-                         label="Git Collaborations" 
-                         /> 
-
-                         <Tab 
-                         className={classes.tab} 
-                         component={Link} to ="/Forums" 
-                         label="Forums" 
-                         /> 
-
-                         <Tab 
-                         className={classes.tab} 
-                         component={Link} to ="/Meetups" 
-                         label="Meetups"/> 
-
-                      </Tabs>
-
-                      <Button 
-                      variant="contained" 
-                      color="secondary" 
-                      className={classes.button}>
-                          Login
-                      </Button>
-
-                      <Menu
-                        id="simple-menu" 
-                        anchorEl={anchorEl} 
-                        open={open}
-                        onClose={handleClose}
-                        classes={{paper: classes.menu}}
-                        MenuListProps={{onMouseLeave: handleClose}}
-                        elevation={0}
-                      >
-
-                     {menuOptions.map((option, i) => (
-                             <MenuItem key={option} component={Link} to={option.link}
-                             classes={{root: classes.menuItem}}
-                             onClick={(event) => {handleMenuItemClick(event,i); setValue(1);
-                            handleClose()}}
-                             selected={i === selectedIndex && value === 1}
-                             >
-                             {option.name}
-                             </MenuItem>
-                     ))}
-
-                      </Menu>
-
-
-                    </Toolbar>
-                </AppBar>
-            </ElevationScroll>
-            <div className={classes.toolbarMargin} />
-        </React.Fragment>
-    )
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
 }
 
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: "3em",
+    [theme.breakpoints.down("nd")]: {
+      marginBottom: "2em",
+    },
+    [theme.breakpoints.down("xs")]: {
+      marginBottom: "1.2em",
+    },
+  },
+  logo: {
+    height: "4em",
+    [theme.breakpoints.down("nd")]: {
+      height: "1em",
+    },
+    [theme.breakpoints.down("xs")]: {
+      height: "2em",
+    },
+  },
+  logoContainer: {
+    padding: 0,
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
+  },
+  tabContainer: {
+    marginLeft: "auto",
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: "25px",
+  },
+  button: {
+    ...theme.typography.signup,
+    borderRadius: "50px",
+    marginLeft: "50 px",
+    marginRight: "20px",
+    height: "45 px",
+  },
+  menu: {
+    backgroundColor: theme.palette.common.arcPurple,
+    color: "white",
+    borderRadius: "0px",
+  },
+  menuItem: {
+    ...theme.typography.tab,
+    opacity: 0.7,
+    "&:hover": {
+      opacity: 1,
+    },
+  },
+  drawerIcon: {
+    height: "50px",
+    width: "50px",
+  },
+  drawerIconContainer: {
+    marginLeft: "auto",
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
+  },
+  drawer: {
+    backgroundColor: theme.palette.common.arcPurple,
+  },
+  drawerItem: {
+    ...theme.typography.tab,
+    color: "white",
+    opacity: 0.7,
+  },
+  drawItemLogin: {
+    backgroundColor: theme.palette.common.arcOrange,
+  },
+  drawItemSelected: {
+    opacity: 1,
+  },
+}));
+
+export default function Header(props) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
+  const matches = useMediaQuery(theme.breakpoints.down("md"));
+
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const [value, setValue] = useState(0);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [openMenu, setOpenMenu] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const handleChange = (e, newValue) => {
+    setValue(newValue);
+  };
+
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+    setOpenMenu(true);
+  };
+
+  const handleMenuItemClick = (e, i) => {
+    setAnchorEl(null);
+    setOpenMenu(false);
+    setSelectedIndex(i);
+  };
+
+  const handleClose = (e) => {
+    setAnchorEl(null);
+    setOpenMenu(false);
+  };
+
+  const menuOptions = [
+    { name: "Git Collaborations", link: "/GitCollaborations" },
+    { name: "Create Git Listing", link: "/CreateGitListing" },
+    { name: "What are Git Collabs?", link: "/AboutGitCollabs" },
+  ];
+
+  useEffect(() => {
+    switch (window.location.pathname) {
+      case "/":
+        if (value !== 0) {
+          setValue(0);
+        }
+        break;
+
+      case "/GitCollaborations":
+        if (value !== 1) {
+          setValue(1);
+          setSelectedIndex(0);
+        }
+        break;
+
+      case "/CreateGitListing":
+        if (value !== 1) {
+          setValue(1);
+          setSelectedIndex(1);
+        }
+        break;
+
+      case "/AboutGitCollabs":
+        if (value !== 1) {
+          setValue(1);
+          setSelectedIndex(2);
+        }
+        break;
+
+      case "/Forums":
+        if (value !== 2) {
+          setValue(2);
+        }
+        break;
+
+      case "/Meetups":
+        if (value !== 3) {
+          setValue(3);
+        }
+        break;
+
+      default:
+        break;
+    }
+  }, [value]);
+
+  const tabs = (
+    <React.Fragment>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        className={classes.tabContainer}
+      >
+        <Tab className={classes.tab} component={Link} to="/" label="Home" />
+
+        <Tab
+          aria-owns={anchorEl ? "simple menu" : undefined}
+          aria-haspopup={anchorEl ? "true" : undefined}
+          className={classes.tab}
+          component={Link}
+          to="/GitCollaborations"
+          onMouseOver={(event) => handleClick(event)}
+          label="Git Collaborations"
+        />
+
+        <Tab
+          className={classes.tab}
+          component={Link}
+          to="/Forums"
+          label="Forums"
+        />
+
+        <Tab
+          className={classes.tab}
+          component={Link}
+          to="/Meetups"
+          label="Meetups"
+        />
+      </Tabs>
+
+      <Button variant="contained" color="secondary" className={classes.button}>
+        Login
+      </Button>
+
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        open={openMenu}
+        onClose={handleClose}
+        classes={{ paper: classes.menu }}
+        MenuListProps={{ onMouseLeave: handleClose }}
+        elevation={0}
+      >
+        {menuOptions.map((option, i) => (
+          <MenuItem
+            key={option}
+            component={Link}
+            to={option.link}
+            classes={{ root: classes.menuItem }}
+            onClick={(event) => {
+              handleMenuItemClick(event, i);
+              setValue(1);
+              handleClose();
+            }}
+            selected={i === selectedIndex && value === 1}
+          >
+            {option.name}
+          </MenuItem>
+        ))}
+      </Menu>
+    </React.Fragment>
+  );
+
+  const drawer = (
+    <React.Fragment>
+      <SwipeableDrawer
+        disableBackdropTransition={!iOS}
+        disableDiscovery={iOS}
+        open={openDrawer}
+        onClose={() => setOpenDrawer(false)}
+        onOpen={() => setOpenDrawer(true)}
+        classes={{ paper: classes.drawer }}
+      >
+        <List disablePadding>
+          <ListItem
+            onClick={() => {
+              setOpenDrawer(false);
+              setValue(0);
+            }}
+            divider
+            button
+            component={Link}
+            to="/"
+            selected={value === 0}
+          >
+            <ListItemText
+              className={
+                value === 0
+                  ? [classes.drawerItem, classes.drawItemSelected]
+                  : classes.drawerItem
+              }
+              disableTypography
+            >
+              Home
+            </ListItemText>
+          </ListItem>
+        </List>
+
+        <List>
+          <ListItem
+            onClick={() => {
+              setOpenDrawer(false);
+              setValue(1);
+            }}
+            divider
+            button
+            component={Link}
+            to="/GitCollaborations"
+            selected={value === 1}
+          >
+            <ListItemText
+              className={
+                value === 1
+                  ? [classes.drawerItem, classes.drawItemSelected]
+                  : classes.drawerItem
+              }
+              disableTypography
+            >
+              Git Collaborations
+            </ListItemText>
+          </ListItem>
+        </List>
+
+        <List>
+          <ListItem
+            onClick={() => {
+              setOpenDrawer(false);
+              setValue(2);
+            }}
+            divider
+            button
+            component={Link}
+            to="/Forums"
+            selected={value === 2}
+          >
+            <ListItemText
+              className={
+                value === 2
+                  ? [classes.drawerItem, classes.drawItemSelected]
+                  : classes.drawerItem
+              }
+              disableTypography
+            >
+              Forums
+            </ListItemText>
+          </ListItem>
+        </List>
+
+        <List>
+          <ListItem
+            onClick={() => {
+              setOpenDrawer(false);
+              setValue(3);
+            }}
+            divider
+            button
+            component={Link}
+            to="/Meetups"
+            selected={value === 3}
+          >
+            <ListItemText
+              className={
+                value === 3
+                  ? [classes.drawerItem, classes.drawItemSelected]
+                  : classes.drawerItem
+              }
+              disableTypography
+            >
+              Meetups
+            </ListItemText>
+          </ListItem>
+        </List>
+
+        <List>
+          <ListItem
+            onClick={() => {
+              setOpenDrawer(false);
+              setValue(4);
+            }}
+            divider
+            button
+            component={Link}
+            className={classes.drawItemLogin}
+            to="/Login"
+            selected={value === 4}
+          >
+            <ListItemText
+              className={
+                value === 4
+                  ? [classes.drawerItem, classes.drawItemSelected]
+                  : classes.drawerItem
+              }
+              disableTypography
+            >
+              Login
+            </ListItemText>
+          </ListItem>
+        </List>
+      </SwipeableDrawer>
+      <IconButton
+        className={classes.drawerContainer}
+        onClick={() => setOpenDrawer(!openDrawer)}
+        disableRipple
+      >
+        <MenuIcon className={classes.drawerIcon} />
+      </IconButton>
+    </React.Fragment>
+  );
+
+  return (
+    <React.Fragment>
+      <ElevationScroll>
+        <AppBar position="fixed" color="primary">
+          <Toolbar disableGutters>
+            <Button
+              component={Link}
+              to="/"
+              disableRipple
+              onClick={() => setValue(0)}
+              className={classes.logoContainer}
+            >
+              <img alt="company logo" className={classes.logo} src={logo} />
+            </Button>
+            {matches ? drawer : tabs}
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </React.Fragment>
+  );
+}
