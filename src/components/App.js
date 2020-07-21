233fe829d8fba8cf6c73b6294fd7f4e0df0aafb2
Rose H <rocio.rosas16@gmail.com>
2020-07-21 14:34:06 +1000

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,1 @@
-import React from 'react';
-import Header from '../components/ui/Header'
-import SignIn from '../components/ui/SignIn'
 
-
-function App() {
-  return (
-    <div className="App"> <Header /> Hello!
-    
-    <SignIn />
-    </div>
-    
-  );
-}
-
-
-export default App;
