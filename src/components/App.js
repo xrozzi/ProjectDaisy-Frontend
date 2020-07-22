7a5035937c397409d0425fad8b2c2d436b0fc3d7
Rose H <rocio.rosas16@gmail.com>
2020-07-22 21:56:01 +1000

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, {useState} from 'react'
 import { ThemeProvider } from "@material-ui/core/styles";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import theme from "./ui/Theme";
 import Header from "../components/ui/Header";
-import LogIn from "../components/ui/LogIn";
+import LogIn from "./LogIn";
+import SignUp from "./SignUp"
 
 function App() {
+  const [userToken, setUserToken] = useState(null)
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Header />
+        <SignUp onLogin={jwt => setUserToken(jwt)}/>
         <LogIn />
         <Switch>
           <Route exact path="/" component={() => <div>Home</div>} />
--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState} from "react"
+import axios from "axios"
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -8,7 +9,7 @@ import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
-// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
@@ -46,16 +47,31 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SignIn() {
+export default function LogIn({onLogin}) {
   const classes = useStyles();
+
+  const [emailValue, setEmailValue] = useState("")
+    const [passwordValue, setPasswordValue] = useState("")
+
+    async function getToken(){
+      const res = await axios.post(`http://localhost:3000/user_token`, {
+          auth: {
+              email: emailValue,
+              password: passwordValue
+          }
+      })
+      onLogin(res.data.jwt)
+  }
 
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>{/* <LockOutlinedIcon /> */}</Avatar>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
         <Typography component="h1" variant="h5">
-          Sign in
+          Log In
         </Typography>
         <form className={classes.form} noValidate>
           <TextField
@@ -65,6 +81,8 @@ export default function SignIn() {
             fullWidth
             id="email"
             label="Email Address"
+            value={emailValue}
+            onChange={e => setEmailValue(e.target.value)}
             name="email"
             autoComplete="email"
             autoFocus
@@ -76,31 +94,30 @@ export default function SignIn() {
             fullWidth
             name="password"
             label="Password"
+            value={passwordValue}
+            onChange={e => setPasswordValue(e.target.value)}
             type="password"
             id="password"
             autoComplete="current-password"
           />
+
           <FormControlLabel
             control={<Checkbox value="remember" color="primary" />}
             label="Remember me"
           />
           <Button
+          onClick={getToken}
             type="submit"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
           >
-            Sign In
+            Log In
           </Button>
           <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="../components/ui/SignUp" variant="body2">
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
