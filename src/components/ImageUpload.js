fc6dd8b5751d74e5fdcca5c9f3723205ef347d99
Nikki Waelkens <nikkiwaelkens@Nikkis-MBP.modem>
2020-07-27 10:28:25 +1000

    upload form updated

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,47 +1,43 @@
 import React, { useState } from "react";
+import App from "./App";
+import { Redirect } from "react-router-dom"
 import axios from "axios";
+import localApi from "../apis/localapi";
 
-export default UploadImage(props) {
-
-
+export default class NewItemForm extends React.Component {
 
     state = {
         image: {}
     }
 
-    onChange = (e) => {
-        e.persist()
+    onChange = (event) => {
+        event.persist()
         this.setState(() => {
             return {
-                [e.target.name]: e.target.file[0]
+                [event.target.name]: event.target.files[0]
             }
         })
     }
 
-    onSubmit = (e) => {
-        e.preventDefault()
+    onSubmit = (event) => {
+        event.preventDefault()
         const form = new FormData()
-        form.append("image", this.state.image))
+        form.append("image", this.state.image)
+        localApi.post(`/images`,
+            { body: form }
+        )
     }
-
     render() {
-        return (<div className="form" >
-            <h1> New Upload </h1>
-
-            <form onSubmit={
-                this.onSubmit
-            }>
-
-                <label> Image Upload </label>
-                <input type="file"
-                    name="image"
-                    onChange={
-                        this.onChange
-                    } />
-
-                <input type="submit" />
-            </form>
-        </div>
+        return (
+            <div className="form">
+                <h1>New Upload</h1>
+                <form onSubmit={this.onSubmit}>
+                    <label>Image Upload</label>
+                    <input type="file" name="image" onChange={this.onChange} />
+                    <br />
+                    <input type="submit" />
+                </form>
+            </div>
         )
     }
 }
