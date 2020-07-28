1785a6d8af3cde3549ea4a47f41b2a0224316589
esthercsoke <esthercsoke@gmail.com>
2020-07-28 22:03:19 +1000

    changes made to gitform testing

--- cypress/integration/gitform.spec.js
+++ cypress/integration/gitform.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-it("adds a new todo item", () => {
+it("create a new git listing", () => {
   cy.visit("localhost:3001/CreateGitListing");
 
   cy.get("#title").type("Post Listing{enter}");
