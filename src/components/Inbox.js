2681080fabcfc4b8e9b604c2aaf40aa764470071
esthercsoke <esthercsoke@gmail.com>
2020-07-29 20:00:00 +1000

    fixed search bar to find user

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+// import UserSearchBar from "./UserSearchBar";
 import ConversationList from "./ConversationList";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
@@ -145,9 +145,7 @@ const Inbox = () => {
         <Grid item>
           <Button
             color="secondary"
-
             onClick={() => {}}
-
             variant="outlined"
             component={Link}
             to={{ pathname: `/Profile` }}
@@ -159,7 +157,6 @@ const Inbox = () => {
       </Grid>
       <Grid container component={Paper} className={classes.chatSection}>
         <Grid item xs={3} className={classes.userSidePanel}>
-
           {/* INPUT CREATEINBOX MSG */}
 
           <Typography variant="h5" className="header-message">
@@ -177,6 +174,8 @@ const Inbox = () => {
                   Find a user to message and then write in a title
                 </DialogContentText>
 
+                {/* <UserSearchBar /> */}
+
                 <TextField
                   autoFocus
                   margin="dense"
@@ -185,7 +184,9 @@ const Inbox = () => {
                   onChange={(e) => setRecieverEmail(e.target.value)}
                   type="text"
                   fullWidth
+                  autoComplete
                 />
+
                 <TextField
                   autoFocus
                   margin="dense"
@@ -274,4 +275,3 @@ const Inbox = () => {
 };
 
 export default Inbox;
-
--- src/components/UserSearchBar.js
+++ src/components/UserSearchBar.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, Component } from "react";
 
 import TextField from "@material-ui/core/TextField";
 
 import Autocomplete from "@material-ui/lab/Autocomplete";
 
 import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
 
 import Toolbar from "@material-ui/core/Toolbar";
 
@@ -12,37 +13,48 @@ import localApi from "../apis/localapi.js";
 
 import axios from "axios";
 
-const UserSearchBar = () => {
-  const [userData, setUserData] = useState([]);
-  const [userEmailId, setUserEmailId] = useState([]);
+export class UserSearchBar extends Component {
+  constructor(props) {
+    super(props);
 
-  useEffect(() => {
+    this.state = {
+      UserData: [],
+      UserIdd: "",
+    };
+  }
+
+  componentDidMount() {
     localApi.get(`/users`).then((response) => {
       console.log(response.data);
-      setUserData(response.data);
+      this.setState({
+        UserData: response.data,
+      });
     });
-  }, []);
+  }
 
-  return (
-    <div style={{ padding: "20px" }}>
-      <Autocomplete
-        className="pding"
-        onChange={setUserEmailId}
-        id="combo-box-demo"
-        options={setUserData}
-        getOptionLabel={(option) => option.email}
-        style={{ width: 300 }}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Auto Complete"
-            variant="outlined"
-            fullWidth
-          />
-        )}
-      />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div style={{ padding: "20px" }}>
+        <Autocomplete
+          className="pding"
+          onChange={this.state.UserIdd.id}
+          id="combo-box-demo"
+          options={this.state.UserData}
+          getOptionLabel={(option) => option.email}
+          style={{ width: 300 }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Auto Complete"
+              variant="outlined"
+              fullWidth
+            />
+          )}
+        />
+        <Button>Find User</Button>
+      </div>
+    );
+  }
+}
 
 export default UserSearchBar;
